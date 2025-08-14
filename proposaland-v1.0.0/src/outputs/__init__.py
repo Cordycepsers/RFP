f8260f92857ad5360de1b2323c1afb182c413641
LeMaja <majalemaja@pm.me>
2025-08-14 02:23:17 +0200

    Delete proposaland-v1.0.0 directory

--- proposaland-v1.0.0/src/outputs/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Output generation modules for Proposaland opportunity monitoring.
-"""
-
-from .excel_generator import ExcelGenerator
-from .json_generator import JSONGenerator
-
-__all__ = ['ExcelGenerator', 'JSONGenerator']
-
